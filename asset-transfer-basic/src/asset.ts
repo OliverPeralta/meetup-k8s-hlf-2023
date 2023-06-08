ceabd94221528b903b9afffe43f0b0ddbedf1b7c
OliverPeralta <olivercp2003@gmail.com>
2023-06-08 00:59:46 -0400

    Changed Variables of assets

--- asset-transfer-basic/src/asset.ts
+++ asset-transfer-basic/src/asset.ts
@@ -2,25 +2,25 @@
   SPDX-License-Identifier: Apache-2.0
 */
 
-import {Object, Property} from 'fabric-contract-api';
+import { Object, Property } from "fabric-contract-api";
 
 @Object()
 export class Asset {
-    @Property()
-    public docType?: string;
+  @Property()
+  public docType?: string;
 
-    @Property()
-    public ID: string;
+  @Property()
+  public ID: string;
 
-    @Property()
-    public Color: string;
+  @Property()
+  public timeStamp: string; // Color
 
-    @Property()
-    public Size: number;
+  @Property()
+  public liters: number; // Size
 
-    @Property()
-    public Owner: string;
+  @Property()
+  public Owner: string;
 
-    @Property()
-    public AppraisedValue: number;
+  @Property()
+  public AppraisedValue: number;
 }
